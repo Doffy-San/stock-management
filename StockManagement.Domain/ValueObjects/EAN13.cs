e2ef84565ca9e6224b2002f5cac009ebb0e1f2a5
Nino Saadi <saadinino@gmail.com>
2026-07-11 19:28:07 +0200

    feat: add domain value objects and enums

---
+++ StockManagement.Domain/ValueObjects/EAN13.cs
@@ -0,0 +1,37 @@
+namespace StockManagement.Domain.ValueObjects;
+
+public class EAN13
+{
+    public string Value { get; private set; }
+
+    public EAN13(string value)
+    {
+        if (!IsValidEAN13(value))
+        {
+            throw new ArgumentException($"'{value}' n'est pas un EAN-13 valide.");
+            throw new ArgumentException($"'{value}' n'est pas un EAN-13 valide.");
+        }
+        Value = value;
+    }
+
+    private static bool IsValid(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value) || value.Length != 13 || !value.All(char.IsDigit))
+            return false;
+
+        int sum = 0;
+        for (int i = 0; i < 12; i++)
+            sum += (value[i] - '0') * (i % 2 == 0 ? 1 : 3);
+
+        int checkDigit = (10 - (sum % 10)) % 10;
+        return checkDigit == (value[12] - '0');
+    }
+
+    public override string ToString() => Value;
+
+    public override bool Equals(object? obj) =>
+        obj is EAN13 other && Value == other.Value;
+
+    public override int GetHashCode() => Value.GetHashCode();
+}
+}
