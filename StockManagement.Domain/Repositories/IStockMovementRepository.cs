bd50c7ab1ff0aa68ab2f428a0f5d94e4781171bf
Nino Saadi <saadinino@gmail.com>
2026-07-11 22:41:04 +0200

    feat: add domain repository interfaces

---
+++ StockManagement.Domain/Repositories/IStockMovementRepository.cs
@@ -0,0 +1,14 @@
+﻿using StockManagement.Domain.Entities;
+using StockManagement.Domain.ValueObjects;
+
+namespace StockManagement.Domain.Repositories;
+
+public interface IArticleRepository
+{
+    Task<Article?> GetByIdAsync(Guid id);
+    Task<Article?> GetByReferenceAsync(EAN13 reference);
+    Task<IEnumerable<Article>> GetAllAsync();
+    Task AddAsync(Article article);
+    Task UpdateAsync(Article article);
+    Task DeleteAsync(Guid id);
+}
